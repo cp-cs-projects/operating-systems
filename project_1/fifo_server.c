091e6ac48081d4b418fd8f15956d2b7e37288a8f
Sri Bala <srirocks2020@sri-mac.local>
2025-01-23 00:41:57 -0800

    resolved additional print

--- project_1/fifo_server.c
+++ project_1/fifo_server.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
 
 #define SIZE 2
 // start server first
@@ -14,6 +15,7 @@ int main(void)
 {
     int fifo;
     char buffer[SIZE];
+    char input[SIZE];
     const char *FIFO = "/tmp/MY_FIFO";
 
     /* Create the FIFO or die trying */
@@ -28,26 +30,22 @@ int main(void)
         return 1;
     }
 
-    ssize_t bytes_read = read(fifo, &buffer, 1); // reads input char
+    /* Input character processing */
+    ssize_t bytes_read = read(fifo, &input, 1);
+    printf("%c -> ", input[0]);
+    bytes_read = read(fifo, &buffer, 1);
+    printf("%c\n", buffer[0]);
 
-
-    while (1)
+    while (bytes_read)
     {
-        if (bytes_read != 1)
-            break;
-
-        printf("%c -> ", buffer[0]); // Print current character
+        printf("%c -> ", buffer[0]);         // Print current character
         bytes_read = read(fifo, &buffer, 1); // Read next character
-
-        if (bytes_read == 1) // Only print the next character if read is successful
-        {
-            printf("%c\n", buffer[0]);
-        }
+        printf("%c\n", buffer[0]);
+        if (buffer[0] == input[0])
+            break;
     }
-    
     /* Read a 0 from the FIFO, so close and delete the FIFO */
     close(fifo);
-    printf("Deleting FIFO\n");
     unlink(FIFO);
 
     return 0;
