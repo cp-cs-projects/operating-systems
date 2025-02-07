5e36e52ebf1f294c13c001b1deed81cfdcb189a5
nam0mi <naominayman@gmail.com>
2025-02-07 11:29:38 -0800

    structure set up

--- project_2/driver.c
+++ project_2/driver.c
@@ -7,12 +7,31 @@ void free_tasks(Task* head);
 void print_stats(Task* head);
 
 int main(int argc, char *argv[]) {
-    
+    int algo;
     if (argc != 3) {
         printf("Usage: %s <filename> <algorithm>\n", argv[0]);
         printf("Algorithms: 1=FCFS, 2=SJF, 3=Priority, 4=RR\n");
         return 1;
     }
+    algo = atoi(argv[2]);
+    switch (algo) {
+    case 1:
+        schedule_fcfs(create_task(1, 2, 3));
+        break;
+    case 2:
+        schedule_sjf(create_task(1, 2, 3));
+        break;
+    case 3:
+        schedule_priority(create_task(1, 2, 3));
+        break;
+    case 4:
+        schedule_rr(create_task(1, 2, 3));
+        break;
+    default:
+        printf("Incorrect algorithm choice.\n");
+        printf("Usage: %s <filename> <algorithm>\n", argv[0]);
+        printf("Algorithms: 1=FCFS, 2=SJF, 3=Priority, 4=RR\n");
+}
     
     return 0;
 }
--- project_2/scheduler.c
+++ project_2/scheduler.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "scheduler.h"
 #include "cpu.h"
+#include "task.h"
 
 void parse_line(char* line, int* tid, int* priority, int* burst) {
     char* token;
@@ -19,4 +20,42 @@ void parse_line(char* line, int* tid, int* priority, int* burst) {
     /* Parse burst */
     token = strsep(&rest, ",");
     *burst = atoi(token);
+}
+
+void schedule_fcfs(Task* head)
+{
+    printf("fcfs\n");
+}
+
+void schedule_sjf(Task* head)
+{
+    printf("sjf\n");
+}
+
+void schedule_priority(Task* head)
+{
+    printf("priority\n");
+}
+
+void schedule_rr(Task* head)
+{
+    printf("round robin\n");
+}
+
+Task* create_task(int id, int burst, int priority)
+{
+    //PLACEHOLDER TO GET EVERYTHING TO COMPILE
+    Task* t = (Task*)malloc(sizeof(Task));
+
+    t->tid = id;
+    t->priority = priority;
+    t->burst = burst;
+    t->original_burst = 0; // IDRK what this should be yet
+    t->waiting_time = 0;
+    t->turnaround_time = 0; // IDRK what this should be yet to start...
+    t->response_time = -1; // maybe a -1 to indicate it hasn't had a chance to run yet
+    t->completed = 0; //not completed
+    t->next = NULL;
+    
+    return t;
 }
